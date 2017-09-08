4a12fca4c905ed26ad6e84343703fff5f4c12a0e
Tiago Rocha <tmrocha89@gmail.com>
2017-09-08 16:50:14 +0100

    predict method implemented, but still not working. train implementation missing

--- AI/controllers/CollaborativeFilteringController.py
+++ AI/controllers/CollaborativeFilteringController.py
@@ -1,6 +1,22 @@
+from repositories.mariadb.RatingsRepositoryMariaDB import RatingsRepositoryMariaDB
+from repositories.mariadb.PlacesRepositoryMariaDB import PlacesRepositoryMariaDB 
+from models.RestrictedBoltzmannMachine import RestrictedBoltzmannMachine
 
+HOST = "maria"
+PORT = 3306
+USER = "fsa"
+PASSWORD = "xourixos"
+DB_NAME = "where2go"
 
 class CollaborativeFilteringController():
+
+    places_df = None
+    ratings_df = None
+    places_ratings_df = None
+    user_group_df = None
+    rbm = None
+    eventsRepo = PlacesRepositoryMariaDB(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB_NAME)
+    ratingsRepo = RatingsRepositoryMariaDB(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB_NAME)
 
     def __init__(self):
         self.__load_data__()
@@ -15,4 +31,8 @@ class CollaborativeFilteringController():
     
     def predict(self, userId):
         print "predicting"
-        return ["res1", "res2", "res3"]
+        if self.rbm is not None:
+            print "running"
+            result = self.rbm.predict(places_df=self.places_df, userId=userId)
+            return result
+        return []

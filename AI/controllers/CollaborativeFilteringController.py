fc4445ceed20df27097713abf63e81d5be571fd9
Tiago Rocha <tmrocha89@gmail.com>
2017-09-09 00:05:46 +0100

    train and predict methods implemented.

--- AI/controllers/CollaborativeFilteringController.py
+++ AI/controllers/CollaborativeFilteringController.py
@@ -15,7 +15,7 @@ class CollaborativeFilteringController():
     places_ratings_df = None
     user_group_df = None
     rbm = None
-    eventsRepo = PlacesRepositoryMariaDB(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB_NAME)
+    placesRepo = PlacesRepositoryMariaDB(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB_NAME)
     ratingsRepo = RatingsRepositoryMariaDB(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB_NAME)
 
     def __init__(self):
@@ -24,9 +24,20 @@ class CollaborativeFilteringController():
 
     def __load_data__(self):
         print "loading data"
+        self.places_df = self.placesRepo.get_dataframe_places()
+        self.ratings_df = self.ratingsRepo.get_dataframe_ratings()
+        print "Total of places registered %s" % len(self.places_df)
+        self.places_ratings_df = self.places_df.merge(self.ratings_df, on='PlaceID') #merge by PlaceID
+        self.places_ratings_df = self.places_ratings_df.drop('Timestamp', axis=1).drop('Title', axis=1).drop('Categories', axis=1)#remove
+        #ToDo: remove the above line, it's only good to check if the data is correct
+        print self.places_ratings_df
+        self.user_group_df = self.places_ratings_df.groupby('UserID')
+        print self.user_group_df.first().head()
 
-    def train(self):
+    def train(self, hidden_layers = 20):
         print "training"
+        self.rbm = RestrictedBoltzmannMachine(hidden_layers = hidden_layers, visible_layers=len(self.places_df))
+        self.rbm.train(users_group=self.user_group_df, places_df=self.places_df, learning_rate=1.0, epochs=15, batch_size=100, total_user_training=1000 )
         return True
     
     def predict(self, userId):

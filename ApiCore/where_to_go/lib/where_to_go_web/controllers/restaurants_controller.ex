73f098461f0065a9120621cd0252b4bbcd939dfe
FARFETCH\bruno.pina <bruno.pina@farfetch.com>
2017-09-09 18:41:32 +0100

    Prediction endpoint sort of implemented

--- ApiCore/where_to_go/lib/where_to_go_web/controllers/restaurants_controller.ex
+++ ApiCore/where_to_go/lib/where_to_go_web/controllers/restaurants_controller.ex
@@ -26,13 +26,11 @@ defmodule WhereToGoWeb.RestaurantsController do
         }       
         
         result = Repo.insert!(poi)
-        IEx.pry
         categories = Enum.map(r["categories"], &tag_to_model(result.id, &1))
         Enum.each(categories, fn x -> Repo.insert!(x) end)
     end
 
     defp tag_to_model(id, t) do
-        IEx.pry
         %Tag{
             point_of_interest_id: id,
             name: t 
@@ -41,13 +39,104 @@ defmodule WhereToGoWeb.RestaurantsController do
 
     def predict(conn, _params) do                                        
         request_url = "http://ai:5000/predict"
-        userid = conn.body_params["userId"]
+        auth_token = get_req_header(conn, "authorization")
 
-        response = HTTPoison.post!(request_url, {:form, [userId: userid]}, %{"Content-type" => "application/x-www-form-urlencoded"}) #URI.encode_query(%{userId: userid}), headers)
-        IEx.pry
-        IO.inspect(response.body, label: "CONN: ")
-        
-        json conn, Poison.decode!(response.body)
+        user = Repo.one(from user in User, where: user.token == ^auth_token)
+        case user do
+            nil -> 
+                conn |> put_status 404
+            _ ->
+                body = {:form, [userId: user.userId]}
+                headers = %{"Content-type" => "application/x-www-form-urlencoded"}
+                response = HTTPoison.post!(request_url, body, headers)
+                decoded_response = Poison.decode!(response.body)
+
+                if length(decoded_response) != 0 do
+                    ids = Enum.map(decoded_response["data"], fn(r) -> List.first(r) end)
+                    
+                    restaurants = Repo.all(
+                        from poi in PointOfInterest,
+                        where: poi.id in ^ids,
+                        limit: 50,
+                        preload: [:tags])
+
+                    filtered_response = Enum.filter(restaurants, &filter_func(_params, &1))
+            
+                    predicted_ratings = Enum.map(decoded_response["data"], 
+                    fn(r) ->
+                        %{id: List.first(r), predicted_rating: List.last(r)} 
+                    end)
+                    encoded_restaurants = Enum.map(filtered_response, &encode_to_map(predicted_ratings, &1))
+                    json conn, Poison.encode!(encoded_restaurants)
+                else
+                    json conn, get_fallback_recommendations(_params)
+                end
+        end
     end
 
+    defp get_fallback_recommendations(_params) do
+        restaurants = Repo.all(
+            from poi in PointOfInterest,
+            order_by: [desc: poi.rating_api],
+            limit: 50,
+            preload: [:tags])
+
+        filtered_response = Enum.filter(restaurants, &filter_func(_params, &1))
+
+        encoded_restaurants = Enum.map(filtered_response, &encode_to_map/1)
+        Poison.encode!(encoded_restaurants)
+    end
+
+    defp encode_to_map(predicted_ratings, f_response) do
+        pr = Enum.find(predicted_ratings, fn(pr) -> pr.id == f_response.id end)
+
+        %{
+            id: f_response.id,
+            url: f_response.url,
+            rating: f_response.rating_api,
+            price: f_response.price,
+            name: f_response.name,
+            image_url: f_response.image_url,
+            categories: Enum.map(f_response.tags, fn(t) -> t.name end),
+            predicted_rating: pr
+        }
+    end
+
+    defp encode_to_map(f_response) do
+        %{
+            id:  f_response.id,
+            url: f_response.url,
+            rating: f_response.rating_api,
+            price: f_response.price,
+            name: f_response.name,
+            image_url: f_response.image_url,
+            categories: Enum.map(f_response.tags, fn(t) -> t.name end)
+        }
+    end
+
+    defp filter_func(filters, r) do
+        filter_city(filters, r) && filter_category(filters, r)
+    end
+
+    defp filter_city(filters, r) do
+        has_city_filter = Map.has_key?(filters, "city")
+
+        if has_city_filter do
+            r.city == filters["city"]
+        else
+            true
+        end
+    end
+
+    defp filter_category(filters, r) do
+        has_category_filter = Map.has_key?(filters, "category")
+
+        if has_category_filter do
+            Enum.any?(r.tags, fn(t) -> t.name == filters["category"] end) 
+        else
+            true
+        end
+    end
   end
+
+  

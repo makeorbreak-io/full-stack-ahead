60afb2be4b4cf5215ee322939f9511134419ba83
Tiago Rocha <tmrocha89@gmail.com>
2017-09-09 12:11:15 +0100

    predict method implemented

--- ApiCore/where_to_go/lib/where_to_go_web/controllers/restaurants_controller.ex
+++ ApiCore/where_to_go/lib/where_to_go_web/controllers/restaurants_controller.ex
@@ -8,4 +8,16 @@ defmodule WhereToGoWeb.RestaurantsController do
     def update_restaurants(conn, _params) do
         text conn, get_restaurants()
     end
+
+    def predict(conn, _params) do                                        
+        request_url = "http://ai:5000/predict"
+        userid = conn.body_params["userId"]
+
+        response = HTTPoison.post!(request_url, {:form, [userId: userid]}, %{"Content-type" => "application/x-www-form-urlencoded"}) #URI.encode_query(%{userId: userid}), headers)
+        IEx.pry
+        IO.inspect(response.body, label: "CONN: ")
+        
+        json conn, Poison.decode!(response.body)
+    end
+
   end
